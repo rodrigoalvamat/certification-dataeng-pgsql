3defcf7038c7c885dd459b55a9594ed0397ba051
Rodrigo de Alvarenga Mattos <rodrigo@bragile.com>
2022-06-03 03:38:49 -0300

    Added requirements.txt

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,14 +5,28 @@ import streamlit as st
 import pandas as pd
 
 # sql libs
-import psycopg2
 from sqlalchemy import create_engine
 
 
-engine = create_engine('postgresql+psycopg2://student:student@127.0.0.1/sparkifydb')
+database = 'postgresql+psycopg2://student:student@127.0.0.1/sparkifydb'
+#database = 'postgresql+psycopg2://erxsjqjd:OhDRrRbv8b59vECc08ENtqtG3rFekShP@heffalump.db.elephantsql.com/erxsjqjd'
 
-query = 'SELECT * FROM songplays'
+
+engine = create_engine(database)
+
+query = """
+SELECT COUNT(*) AS plays, u.first_name AS user
+FROM songplays AS s
+JOIN users AS u
+ON s.user_id = u.user_id
+GROUP BY s.user_id, u.first_name
+ORDER BY plays DESC;
+"""
 
 songplays = pd.read_sql(query, engine)
 
-songplays
+st.write("""
+# Sparkify Dashboard
+""")
+
+st.bar_chart(songplays.head(20))
